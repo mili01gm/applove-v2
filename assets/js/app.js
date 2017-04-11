aa4c1222067521e364e656deee7ba7921a210543
Luz Gutierrez <mili01gm@gmail.com>
2017-04-11 14:19:35 -0500

    Ja! logre el modal con jsgit add . adios chancacon css!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,29 @@
 var fotos = ["Producto 1","Producto 2","Producto 3","Producto 4","Producto 5","Producto 6","Producto 7","Producto 8","Producto 9","Producto 10","Producto 11","Producto 12"];
-
+var modal = document.getElementById('modal');
 function newSrc(num){
   return "assets/img/img-"+(num+1)+".jpg"
-}
-
-function openModal(param){
-  document.getElementById('img01').setAttribute("src",newSrc(param));
-  document.getElementById('span01').innerText = fotos[param]
 }
 
 function newProduct(elem){
   var div = document.createElement("div");
   div.setAttribute("id","grid-products");
   var aPic = document.createElement("a");
-  aPic.setAttribute("href","#modal");
   aPic.setAttribute("class","main-img");
   var pic = document.createElement("img");
   var indice = fotos.indexOf(elem);
   pic.setAttribute("src", newSrc(indice));
   pic.setAttribute("width","320");
-  pic.addEventListener("click",openModal(indice));
+  pic.addEventListener("click",function(e){
+    e.preventDefault();
+    var modal = document.getElementsByClassName('modal')[0];
+    document.getElementById("img01").setAttribute("src",this.src);
+    modal.style.display = "block";
+    if(modal.style.display == "block"){
+      document.getElementById('close').addEventListener("click",function(e){
+        modal.style.display = "none";
+      })
+    }
+  });
   var span = document.createElement("span");
   span.setAttribute("class","main-span nb-0");
   span.innerText = fotos[indice]
